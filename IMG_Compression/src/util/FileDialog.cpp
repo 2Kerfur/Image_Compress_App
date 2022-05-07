179d2e95dca191e496f1127e21b73b3c85b27b27
2Kerfur <fidotovm06@gmail.com>
2022-05-07 11:23:58 -0700

    Added file dialog class

--- IMG_Compression/src/util/FileDialog.cpp
+++ IMG_Compression/src/util/FileDialog.cpp
@@ -63,3 +63,33 @@ LPCWSTR FileDialog::OpenFile(HWND hWnd)
 	return file_path;
 }
 
+LPCWSTR FileDialog::OpenFolder(HWND hWnd)
+{
+	TCHAR path[MAX_PATH];
+
+	const char* path_param = saved_path.c_str();
+
+	BROWSEINFO bi = { 0 };
+	bi.lpszTitle = ("Browse for folder...");
+	bi.ulFlags = BIF_RETURNONLYFSDIRS | BIF_NEWDIALOGSTYLE;
+	bi.lpfn = BrowseCallbackProc;
+	bi.lParam = (LPARAM)path_param;
+
+	LPITEMIDLIST pidl = SHBrowseForFolder(&bi);
+
+	if (pidl != 0)
+	{
+		//get the name of the folder and put it in path
+		SHGetPathFromIDList(pidl, path);
+
+		//free memory used
+		IMalloc* imalloc = 0;
+		if (SUCCEEDED(SHGetMalloc(&imalloc)))
+		{
+			imalloc->Free(pidl);
+			imalloc->Release();
+		}
+
+		return path;
+	}
+}
